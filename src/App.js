128af6560648f36dce48ca109f6dc9803f581e6b
petervkay <petervkay@gmail.com>
2018-11-02 12:50:26 -0500

    desktop styled!

--- src/App.js
+++ src/App.js
@@ -12,41 +12,43 @@ let Calculator = (props) => {
 
   return (
     <form className="form">
-      <div className='title container'>
+      <div className='title align-left'>
         <h2>SurgicAir™ ZERO</h2>
         <h1>SSI Prevention Calculator</h1>
+        <hr />
       </div>
-      <div className='row border-bottom container'>
-        <div className='two-column'>
-          <h2>Calculate the human benefits and ROI of the SurgicAir ZERO Airflow System.</h2>
-          <p>Airborne pathogens are a proven and significant source of SSIs, contributing substantially to surgical morbidity and mortality each year. The first step to this problem is in their prevention, and that’s where SurgicAir™ Zero Airflow comes in.</p>
+      <div className='row'>
+        <div className='two-column align-left'>
+          <h2 style={{"font-size":"30px","padding":"50px 0"}}>Calculate the human benefits and ROI of the SurgicAir ZERO Airflow System.</h2>
+          <p className='gray'> Airborne pathogens are a proven and significant source of SSIs, contributing substantially to surgical morbidity and mortality each year. The first step to this problem is in their prevention, and that’s where SurgicAir™ Zero Airflow comes in.</p>
         </div>
         <div className='two-column'>
           <div className = 'totalAnnualCases'>
-            <h3> Calculate total annual SSI cases per O.R. </h3>
+            <h3 style={{"font-size":"24px","text-transform":"uppercase", "padding-top":"50px"}}> Calculate total annual SSI cases per O.R. </h3>
             <div className='automatic container'>
-              <div className="field">
                 <div className="control independent">
-                  <label className="label">Days Per Week Used</label>
                   <Field name="daysPerWeek" component={CustomInput} customValues={props.customValues} />
+                  <label className="label white">Days Per Week One O.R. is Used</label>
                 </div>
+                <span className='times white'>×</span>
                 <div className="control independent">
-                  <label className="label">Cases Per Day</label>
                   <Field name="casesPerDay" component={CustomInput} customValues={props.customValues} />
+                  <label className="label white">Cases Per Day For One O.R.</label>                  
                 </div>
+                <span className='times white'>×</span>
                 <div className="control independent">
-                  <label className="label">Weeks Per Year Used</label>
                   <Field name="weeksPerYear" component={CustomInput} customValues={props.customValues} />
+                  <label className="label white">Weeks Per Year One O.R. is Used</label>
                 </div>
-              </div>
             </div>
             <div className='manual hybrid container'>
-              <label className="label">Or, Enter Total Annual cases</label>
+              <label className="label white inline">Or, Enter Total Annual Cases for one O.R.</label> 
               <Field name="totalAnnualCases" component={CustomInput} customValues={props.customValues}/>
             </div>
           </div>
         </div>
       </div>
+      <hr style={{"margin-top":"60px", "margin-bottom":"40px"}} />
       <Results {...props} customValues={props.customValues} />
     </form>
   ) // end return
@@ -91,7 +93,7 @@ class App extends Component {
       daysPerWeek: "",
       casesPerDay: "",
       weeksPerYear: "",
-      totalAnnualCases: "",
+      totalAnnualCases: "400",
       SSIrate: "45",
       averageCaseCost: "10",
       numberORs: "20",
--- src/results.js
+++ src/results.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {Field} from 'redux-form';
 import CustomInput from './customInput.js';
 import Dependent from './dependent.js';
+import surgicair from './assets/surgicair.png'
 
 export default class Results extends Component {
 	render() {
@@ -9,92 +10,119 @@ export default class Results extends Component {
 		const reductionValues = [cv.column15SSIreduction,cv.column30SSIreduction,cv.column45SSIreduction];
 		return(
 		<div className='results'>
-	        <h1> Your Results </h1>
+	        <h1 style={{"font-weight":"normal", "margin-bottom":"40px"}} className="align-left"> Your Results </h1>
 	        <div className='row'>
 	          <div className='five-column assumptions'>
 	            <h3 className='green'> Assumptions </h3>
-	            <div className='container green'>
-	              <label className='label large'>SSI Rate </label>
+	            <div className='container bg-green small-input'>
+	              <label className='label large white'>SSI Rate </label>
 	              <Field name='SSIrate' component={CustomInput}  suffix='%' />
 	            </div>
-	            <div className='container green'>
-	              <label className='label'>Average SSI Case Cost Per O.R.</label>
+	            <div className='container bg-green'>
+	              <label className='label white'>Average SSI Case Cost Per O.R.</label>
 	              <Field name='averageCaseCost' component={CustomInput} prefix='$' />
 	            </div>
-	            <div className='container green'>
-	              <label className='label'>Number of O.R.s</label>
+	            <div className='container bg-green'>
+	              <label className='label white'>Number of O.R.s</label>
 	              <Field name='numberORs' component={CustomInput} />
 	            </div>
 	          </div>
 	        <div className='five-column cla'>
-	            <h3 className='black'>Compliant Laminar Airflow</h3>
-	            <div className='bg-gray'>
-		            <p className='color-white'>
+	            <h3 className='black' style={{"line-height":"27px"}}>Compliant<br /> Laminar Airflow</h3>
+	            <div class='container'>
+		            <p className='white bg-gray cla-intro'>
 		                Complaint Laminar Airflow is what your O.R. has if it doesn't have SurgicAir Zero
 		            </p>
-		            <label className='label'>Number of SSIs</label>
-	{	            <Dependent 
-						name='numberSSIs' 
-						value = {Math.ceil(cv.totalAnnualCases * (cv.SSIrate / 100) * cv.numberORs)} 
-					  />
-	}	            <label className='label'>Annual Total SSI Cost</label>
-		            <Dependent 
-		            	name='CLAannualTotalSSICost' 
-		            	value = {Math.ceil(cv.totalAnnualCases * (cv.SSIrate / 100) * cv.numberORs * cv.averageCaseCost)} 
-		            	prefix = '$' 
-		            />
-	        	</div>
+		            <div className='dependent large-value'>
+			            <label className='label '>Number of SSIs</label>
+			            <Dependent 
+							name='numberSSIs' 
+							value = {Math.ceil(cv.totalAnnualCases * (cv.SSIrate / 100) * cv.numberORs)} 
+						  />
+					</div>
+					<div className='dependent'>
+			            <label className='label'>Annual Total SSI Cost</label>
+			            <Dependent 
+			            	name='CLAannualTotalSSICost' 
+			            	value = {Math.ceil(cv.totalAnnualCases * (cv.SSIrate / 100) * cv.numberORs * cv.averageCaseCost)} 
+			            	prefix = '$' 
+			            />
+			        </div>
+		         </div>
 	         </div>
 	          {[15,30,45].map(function(percentage, index){
 
 	          	return (
 	          		<div className='five-column' key={index}>
-	          			<img src='/../../surgicair.png'/>
-	          			<label className='label'>Enter SSI Reduction Rate</label>
-	          			<Field name={"column" + percentage + "SSIreduction"} component={CustomInput} suffix="%" />
-	          			<label className='label'>Number of SSIs</label>
-	          			<Dependent
-	          				name={'numberSSIs'+percentage}
-	          				value={Math.ceil(cv.totalAnnualCases * (cv.SSIrate / 100) * cv.numberORs* (1-(reductionValues[index] / 100)))}
-	          			/>
-	          			<label className='label'>Annual Total SSI Cost</label>
-	          			<Dependent 
-	          				name={'annualTotalSSICost' + percentage}
-	          				value= {Math.ceil(cv.totalAnnualCases * (cv.SSIrate / 100) * cv.numberORs * cv.averageCaseCost * (1-(reductionValues[index] / 100)))}
-	          				prefix= "$"
-	          			/>
-	          			<label className='label'>SSIs Prevented</label>
-	          			<Dependent
-	          				name={"SSIsPrevented"+percentage}
-	          				value = {
-	          					Math.ceil(cv.totalAnnualCases * (cv.SSIrate / 100) * cv.numberORs* (reductionValues[index] / 100))
-	          				}
-	          			/>
-	          			<label className='label'>Annual Savings</label>
-	          			<Dependent
-	          				name={"TotalAnnualSavings"+percentage}
-	          				value = {Math.ceil(cv.totalAnnualCases * (cv.SSIrate / 100) * cv.numberORs * cv.averageCaseCost * (reductionValues[index] / 100))}
-	          			/>
-	          			<label className='label'>ROI</label>
-	          			<Dependent
-	          				name={"ROI"+percentage}
-	          				value = {
-	          					(12*250000/
-	          					Math.ceil(cv.totalAnnualCases * (cv.SSIrate / 100) * cv.numberORs * cv.averageCaseCost * (reductionValues[index] / 100)))
-	          					.toFixed(1)
-	          				}
-	          			/>
-	          			<label className='label'>Future Savings</label>
-	          			<Dependent
-	          				name={'futureSavings'+percentage}
-	          				value={
-	          					20
-	          					*
-	          					Math.ceil(cv.totalAnnualCases * (cv.SSIrate / 100) * cv.numberORs * cv.averageCaseCost * (reductionValues[index] / 100))
-	          				}
-	          			/>
-
-
+	          			<img src={surgicair} alt="SurgicAir Zero Logo" className='surgicair-logo'/>
+	          			<div className='container reduction'>
+	          				<div class='rate bg-blue'>
+			          			<label className='label white'>Enter SSI Reduction Rate</label>
+			          			<Field name={"column" + percentage + "SSIreduction"} component={CustomInput} suffix="%" />
+			          		</div>
+			          		<div class='section'>
+			          			<div className='dependent large-value'>
+				          			<label className='label'>Number of SSIs</label>
+				          			<Dependent
+				          				name={'numberSSIs'+percentage}
+				          				value={Math.ceil(cv.totalAnnualCases * (cv.SSIrate / 100) * cv.numberORs* (1-(reductionValues[index] / 100)))}
+				          			/>
+				          		</div>
+				          		<div className='dependent'>
+				          			<label className='label'>Annual Total SSI Cost</label>
+				          			<Dependent 
+				          				name={'annualTotalSSICost' + percentage}
+				          				value= {Math.ceil(cv.totalAnnualCases * (cv.SSIrate / 100) * cv.numberORs * cv.averageCaseCost * (1-(reductionValues[index] / 100)))}
+				          				prefix= "$"
+				          			/>
+			          			</div>
+			          		</div>
+			          		<div class='section'>
+			          			<div className='dependent'>
+				          			<label className='label'>SSIs Prevented</label>
+				          			<Dependent
+				          				name={"SSIsPrevented"+percentage}
+				          				value = {
+				          					Math.ceil(cv.totalAnnualCases * (cv.SSIrate / 100) * cv.numberORs* (reductionValues[index] / 100))
+				          				}
+				          			/>
+			          			</div>
+			          			<div className='dependent'>
+				          			<label className='label'>Annual Savings</label>
+				          			<Dependent
+				          				name={"TotalAnnualSavings"+percentage}
+				          				value = {Math.ceil(cv.totalAnnualCases * (cv.SSIrate / 100) * cv.numberORs * cv.averageCaseCost * (reductionValues[index] / 100))}
+				          			/>
+				          		</div>
+				          	</div>
+				          	<div class='section'>
+			          			<div className='dependent roi'>
+				          			<label className='label'>ROI</label>
+				          			<div className='circle'>
+					          			<Dependent
+					          				name={"ROI"+percentage}
+					          				value = {
+					          					(12*250000/
+					          					Math.ceil(cv.totalAnnualCases * (cv.SSIrate / 100) * cv.numberORs * cv.averageCaseCost * (reductionValues[index] / 100)))
+					          					.toFixed(1)
+					          				}
+					          			/>
+					          			<span className='months'>months</span>
+					          		</div>
+				          		</div>
+			          			<div className='dependent'>
+				          			<label className='label'>Future Savings</label>
+				          			<Dependent
+				          				name={'futureSavings'+percentage}
+				          				value={
+				          					20
+				          					*
+				          					Math.ceil(cv.totalAnnualCases * (cv.SSIrate / 100) * cv.numberORs * cv.averageCaseCost * (reductionValues[index] / 100))
+				          				}
+				          			/>
+				          		</div>
+				          	</div>
+			          	</div>
 	          		</div>
 	          	)
 	          })}
